1071f8b437a068f39bd7d76c675fa00ed2d276e9
Beatriz Amaral <beatrizamaral81@gmail.com>
2026-01-21 13:52:26 -0300

    fix(Eslint): add coment

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -119,6 +119,7 @@ const Dashboard = () => {
         fetchWhatsappStatus();
 
         return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
