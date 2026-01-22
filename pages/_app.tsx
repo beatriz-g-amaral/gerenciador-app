2436a34777e7469acaea604362b65dceade9e9c6
Beatriz Amaral <beatrizamaral81@gmail.com>
2026-01-21 23:10:34 -0300

    fix(Code): Remove frrom src

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../App.css';
-import '../components/Dashboard.css';
-import '../components/Chatbot.css';
+import '../src/App.css';
+import '../src/components/Dashboard.css';
+import '../src/components/Chatbot.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
